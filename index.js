44a3d568828a2ca933cb550643e428f3f203da54
Muhammad-Ammad3 <ammad0598@gmail.com>
2024-12-26 23:18:46 +0500

    second commit

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 const PORT = 4000
 
+const tasks = [
+    {
+        id : 1,
+        task : "complete project"
+    },
+    {
+        id : 2,
+        task : "push github"
+    },
+    {
+        id : 3,
+        task : "diploy vercel"
+    }
+]
+
+
+app.use(morgan("tiny"))
+
+function middleware(req,res,next){
+    req.requestBy = "Muhammad Ammad";
+    next()
+}
+
+app.use(express.json())
+app.use(middleware)
+
 app.get("/",(req,res)=>{
-    console.log(req);
-    res.send("Hello World to first API");
+    console.log("req.requestBy=>",req.requestBy);
     
+    res.status(200).send(tasks);
+})
+app.post("/",(req,res)=>{
+    console.log("req.body=>",req.body)
+    
+    res.send("Post request called succesfully");
+})
+app.put("/",(req,res)=>{
+    res.send("Put request called succesfully");
+})
+app.delete("/",(req,res)=>{
+    res.send("Deleted request called succesfully");
 })
 
 app.listen(PORT , ()=> console.log("Sesrver is running on port" + PORT))
